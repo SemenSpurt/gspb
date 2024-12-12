aff30e633f3d00d8b784b8e8ae4d2ac71f2d2077
SemenSpurt <spurtsemen@gmail.com>
2024-12-12 10:04:01 +0300

    priv/repo/seeds.ex clear up

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -10,54 +10,54 @@
 # We recommend using the bang functions (`insert!`, `update!`
 # and so on) as they will fail if something goes wrong.
 
-alias Feed.Route.{
-  Routes,
-  Trips
-}
-alias Feed.Place.{
-  Stops,
-  Shapes
-}
+# alias Feed.Route.{
+#   Routes,
+#   Trips
+# }
+# alias Feed.Place.{
+#   Stops,
+#   Shapes
+# }
 
-alias Feed.Time.{
-  Dates,
-  Freqs,
-  Week,
-  Times
-}
-
-
-NimbleCSV.define(Parser, separator: ",", escape: "\"")
+# alias Feed.Time.{
+#   Dates,
+#   Freqs,
+#   Week,
+#   Times
+# }
 
 
-tables = [
-  {Dates, "/src/feed/calendar_dates.txt"},
-  {Freqs, "/src/feed/frequencies.txt"},
-  {Times, "/src/feed/stop_times.txt"},
-  {Routes, "/src/feed/routes.txt"},
-  {Shapes, "/src/feed/shapes.txt"},
-  {Week, "/src/feed/calendar.txt"},
-  {Trips, "/src/feed/trips.txt"},
-  {Stops, "/src/feed/stops.txt"},
-]
+# NimbleCSV.define(Parser, separator: ",", escape: "\"")
 
-unzipped = File.ls!("src")
-  |> Enum.map(&String.ends_with?(&1, ".txt"))
-  |> Enum.any?
 
-if not unzipped do
-  :zip.unzip(~c"src/feed.zip", [{:cwd, ~c"src/feed"}])
-end
+# tables = [
+#   {Dates, "/src/feed/calendar_dates.txt"},
+#   {Freqs, "/src/feed/frequencies.txt"},
+#   {Times, "/src/feed/stop_times.txt"},
+#   {Routes, "/src/feed/routes.txt"},
+#   {Shapes, "/src/feed/shapes.txt"},
+#   {Week, "/src/feed/calendar.txt"},
+#   {Trips, "/src/feed/trips.txt"},
+#   {Stops, "/src/feed/stops.txt"},
+# ]
 
-Enum.each(tables, fn {model, path} ->
-  records = File.cwd! <> path
-  |> File.stream!()
-  |> Stream.chunk_every(1000)
-  |> Enum.map(&Parser.parse_stream(&1) |> model.import())
+# unzipped = File.ls!("src")
+#   |> Enum.map(&String.ends_with?(&1, ".txt"))
+#   |> Enum.any?
 
-end)
+# if not unzipped do
+#   :zip.unzip(~c"src/feed.zip", [{:cwd, ~c"src/feed"}])
+# end
 
-File.ls!("src/feed")
-|> Enum.filter(&String.ends_with?(&1, ".txt"))
-|> Enum.map(&Path.expand(&1, "src/feed"))
-|> Enum.map(&File.rm!(&1))
+# Enum.each(tables, fn {model, path} ->
+#   records = File.cwd! <> path
+#   |> File.stream!()
+#   |> Stream.chunk_every(1000)
+#   |> Enum.map(&Parser.parse_stream(&1) |> model.import())
+
+# end)
+
+# File.ls!("src/feed")
+# |> Enum.filter(&String.ends_with?(&1, ".txt"))
+# |> Enum.map(&Path.expand(&1, "src/feed"))
+# |> Enum.map(&File.rm!(&1))
